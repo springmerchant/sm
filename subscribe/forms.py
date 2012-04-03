c636b33e5aed138fb7446d14dc070d46ac915ae2
Emilian Felecan <emil@springmerchant>
2012-04-03 18:29:16 +0300

    Removing the fieldset title.

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -3,7 +3,7 @@ from bootstrap2.forms import BootstrapForm, Fieldset
 
 class SubscribeForm(BootstrapForm):
     class Meta:
-        layout = (Fieldset("Enter Your Email Address", "email", "first_name"),)
+        layout = (Fieldset("", "email", "first_name"),)
 
     email = forms.EmailField(label="Email")
     first_name = forms.CharField(label="First Name",max_length=256)
