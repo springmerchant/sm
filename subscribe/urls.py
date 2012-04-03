108879116ad0c86ad6c9e112a723d324610dc390
Emilian Felecan <emil@springmerchant>
2012-04-03 14:42:51 +0300

    Correcting import statement.

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from sb.subscribe.views import SubscribeView, SuccessView
+from sm.subscribe.views import SubscribeView, SuccessView
 
 urlpatterns = patterns('',
     url(r'^$', SubscribeView.as_view(), name='subscribe_view'),
